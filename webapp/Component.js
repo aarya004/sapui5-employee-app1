827ee91252f6e32196142afdb7d6daeea2427c87
aarya004 <141934396+aarya004@users.noreply.github.com>
2026-03-14 16:04:15 +0530

    Add initial Component.js for UIComponent extension

---
+++ webapp/Component.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+ "sap/ui/core/UIComponent"
+], function (UIComponent) {
+ "use strict";
+
+ return UIComponent.extend("demo.Component", {
+
+  metadata: {
+   rootView: {
+    viewName: "demo.view.Main",
+    type: "XML"
+   }
+  }
+
+ });
+
+});
